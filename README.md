cf5a28f1e1379a89be281bb8649a7ce3ac4cbbc1
Dzoara Bello <sambelmor@gmail.com>
2018-10-08 09:44:13 -0500

    add todo app
    - It was created a CRUD application
    - The user can log in on the application

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+This is an example of todo list
+
+* You can: Create, Read, Update and Delete the tasks that you want.
---
+++ app/models/item.rb
@@ -0,0 +1,16 @@
+class Item < ApplicationRecord
+  belongs_to :user
+
+  def completed?
+    !completed_at.blank?
+  end
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |result|
+        csv << result.attributes.values_at(*column_names)
+      end
+    end
+  end
+end
