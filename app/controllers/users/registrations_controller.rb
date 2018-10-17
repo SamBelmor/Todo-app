2b832925c318656c2ecbec682c7d3609ea93ca53
Dzoara Bello <sambelmor@gmail.com>
2018-10-17 10:47:08 -0500

    add name and description migrations
    - A new migration of user name and description was added

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:emai, :password, :password_confirmation, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :description, :password, :password_confirmation, :avatar])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:emai, :password, :password_confirmation, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :name, :description, :password, :password_confirmation, :avatar])
   end
 
   # The path used after sign up.
