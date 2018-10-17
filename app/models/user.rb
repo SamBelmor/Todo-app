3a3f9c8f08d8b69523f81a9abb499fc295bc0b82
Dzoara Bello <sambelmor@gmail.com>
2018-10-16 19:27:35 -0500

    add validate when the user doesn't upload an image
    - It was added an image by default when the user doesn't upload and image on his profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ApplicationRecord
   has_many :items
   has_one_attached :avatar
+  before_save :attach_image
   after_create :send_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+
+  def attach_image
+    unless self.avatar.attachment.present?
+      self.avatar.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default_avatar.png')), 
+      filename: 'default_avatar.png', 
+      content_type: 'image/png')
+    end
+  end
 
   private      
   def send_email
